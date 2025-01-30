7fa2f18d0ce075c9951c44ce4f8d07a6f117f303
Nicolas <nicoesve@gmail.com>
2025-01-30 12:01:17 -0500

    cosmetic changes

--- R/other_utils.R
+++ R/other_utils.R
@@ -1,11 +1,11 @@
 #' Metric Object Constructor
 #'
-#' Constructs a metric object that contains the necessary functions for Riemannian operations.
+#' Constructs a metric object that contains the necessary functions for Riemannian operations.# nolint: line_length_linter.
 #'
-#' @param log A function representing the Riemannian logarithmic map. This function should accept a `dppMatrix` (the reference point) and another `dppMatrix` (the matrix whose logarithm is to be computed), and it outputs a `dspMatrix` (the tangent image).
-#' @param exp A function representing the Riemannian exponential map. This function should accept a `dppMatrix` (the reference point) and a `dspMatrix` (the matrix whose exponential is to be computed) and return a `dppMatrix` (the image on the manifold).
-#' @param vec A function representing the vectorization operation for tangent spaces. This function should accept a `dppMatrix` (the reference point) and a `dspMatrix` (the tangent image) and return a vector (the vectorized image).
-#' @param unvec A function representing the inverse of the vectorization operation. This function should accept a `dppMatrix` (the reference point) and a vector (the vectorized image), and it returns a `dspMatrix` (the tangent image).
+#' @param log A function representing the Riemannian logarithmic map. This function should accept a `dppMatrix` (the reference point) and another `dppMatrix` (the matrix whose logarithm is to be computed), and it outputs a `dspMatrix` (the tangent image).# nolint: line_length_linter.
+#' @param exp A function representing the Riemannian exponential map. This function should accept a `dppMatrix` (the reference point) and a `dspMatrix` (the matrix whose exponential is to be computed) and return a `dppMatrix` (the image on the manifold).# nolint: line_length_linter.
+#' @param vec A function representing the vectorization operation for tangent spaces. This function should accept a `dppMatrix` (the reference point) and a `dspMatrix` (the tangent image) and return a vector (the vectorized image).# nolint: line_length_linter.
+#' @param unvec A function representing the inverse of the vectorization operation. This function should accept a `dppMatrix` (the reference point) and a vector (the vectorized image), and it returns a `dspMatrix` (the tangent image).# nolint: line_length_linter.
 #'
 #' @return An object of class `rmetric` containing the specified functions.
 #' @export
@@ -17,11 +17,11 @@ metric <- function(log, exp, vec, unvec) {
 
 #' Generate Random Samples from a Riemannian Normal Distribution
 #'
-#' Simulates random samples from a Riemannian normal distribution on symmetric positive definite matrices.
+#' Simulates random samples from a Riemannian normal distribution on symmetric positive definite matrices.# nolint: line_length_linter.
 #'
 #' @param n Number of samples to generate.
-#' @param refpt Reference point on the manifold, represented as a symmetric positive definite matrix. Must be an object of class `dppMatrix` from the Matrix package.
-#' @param disp Dispersion matrix defining the spread of the distribution. Must be an object of class `dppMatrix` from the Matrix package.
+#' @param refpt Reference point on the manifold, represented as a symmetric positive definite matrix. Must be an object of class `dppMatrix` from the Matrix package.# nolint: line_length_linter.
+#' @param disp Dispersion matrix defining the spread of the distribution. Must be an object of class `dppMatrix` from the Matrix package.# nolint: line_length_linter.
 #' @param met A metric object of class `rmetric`.
 #'
 #' @return An object of class `CSample` containing the generated samples.
@@ -37,14 +37,14 @@ rspdnorm <- function(n, refpt, disp, met) {
 
 #' Relocate Tangent Representations to a New Reference Point
 #'
-#' Changes the reference point for tangent space representations on a Riemannian manifold.
+#' Changes the reference point for tangent space representations on a Riemannian manifold.# nolint: line_length_linter.
 #'
-#' @param old_ref A reference point on the manifold to be replaced. Must be an object of class `dppMatrix` from the Matrix package.
-#' @param new_ref The new reference point on the manifold. Must be an object of class `dppMatrix` from the Matrix package.
-#' @param images A list of tangent representations relative to the old reference point. Each element in the list must be an object of class `dspMatrix`.
-#' @param met A metric object of class `rmetric`, containing functions for Riemannian operations (logarithmic map, exponential map, vectorization, and inverse vectorization).
+#' @param old_ref A reference point on the manifold to be replaced. Must be an object of class `dppMatrix` from the Matrix package.# nolint: line_length_linter.
+#' @param new_ref The new reference point on the manifold. Must be an object of class `dppMatrix` from the Matrix package.# nolint: line_length_linter.
+#' @param images A list of tangent representations relative to the old reference point. Each element in the list must be an object of class `dspMatrix`.# nolint: line_length_linter.
+#' @param met A metric object of class `rmetric`, containing functions for Riemannian operations (logarithmic map, exponential map, vectorization, and inverse vectorization).# nolint: line_length_linter.
 #'
-#' @return A list of tangent representations relative to the new reference point. Each element in the returned list will be an object of class `dspMatrix`.
+#' @return A list of tangent representations relative to the new reference point. Each element in the returned list will be an object of class `dspMatrix`.# nolint: line_length_linter.
 #' @export
 relocate <- function(old_ref, new_ref, images, met) {
     images |> furrr::future_map(
@@ -54,58 +54,67 @@ relocate <- function(old_ref, new_ref, images, met) {
 
 #' Compute the Frechet Mean
 #'
-#' This function computes the Frechet mean of a sample using an iterative algorithm.
+#' This function computes the Frechet mean of a sample using an iterative algorithm.# nolint: line_length_linter.
 #'
 #' @param sample An object of class `CSample` containing the sample data.
-#' @param tol A numeric value specifying the tolerance for convergence. Default is 0.05.
-#' @param max_iter An integer specifying the maximum number of iterations. Default is 20.
+#' @param tol A numeric value specifying the tolerance for convergence. Default is 0.05.# nolint: line_length_linter.
+#' @param max_iter An integer specifying the maximum number of iterations. Default is 20.# nolint: line_length_linter.
 #' @param lr A numeric value specifying the learning rate. Default is 0.2.
 #'
-#' @return The function updates the `frechet_mean` field of the `sample` object with the computed Frechet mean.
+#' @return The function updates the `frechet_mean` field of the `sample` object with the computed Frechet mean.# nolint: line_length_linter.
 #'
 #' @details
-#' The function iteratively updates the reference point of the sample until the change in the reference point is less than the specified tolerance or the maximum number of iterations is reached. If the tangent images are not already computed, they will be computed before starting the iterations.
+#' The function iteratively updates the reference point of the sample until the change in the reference point is less than the specified tolerance or the maximum number of iterations is reached. If the tangent images are not already computed, they will be computed before starting the iterations.# nolint: line_length_linter.
 #'
 #' @examples
 #' \dontrun{
-#' sample <- CSample$new(conns = list_of_dppMatrix_objects, metric = some_metric)
+#' sample <- conn_sample$new(
+#'     conns = list_of_dppMatrix_objects,
+#'     metric = some_metric
+#' )
 #' compute_fmean(sample, tol = 0.01, max_iter = 50, lr = 0.1)
 #' }
 compute_fmean <- function(sample, tol = 0.05, max_iter = 20, lr = 0.2) {
-    if (!is.null(sample$frechet_mean)) { 
+    if (!is.null(sample$frechet_mean)) {
         warning("The Frechet mean has already been computed.")
     }
-    if (is.null(sample$tangent_images)) { 
+    if (is.null(sample$tangent_images)) {
         message("tangent images were null, so they will be computed")
         sample$compute_tangents()
     }
     if (!is.numeric(tol)) stop("tol must be a numeric.")
     if (max_iter < 1) stop("max_iter must be at least 1.")
 
-    aux_sample <- sample; delta <- Inf; iter <- 0
+    aux_sample <- sample
+    delta <- Inf
+    iter <- 0
 
     while ((delta > tol) && (iter < max_iter)) {
-        old_tan <- sample$tangent_images; iter <- iter + 1 
+        old_tan <- sample$tangent_images
+        iter <- iter + 1
         old_ref_pt <- sample$tangent_handler$reference_point
 
         if (iter > max_iter) {
-            warning("Computation of Frechet mean exceeded maximum 
+            warning("Computation of Frechet mean exceeded maximum
                 number of iterations.")
         }
 
-        tan_step <- lr * Reduce(`+`, old_tan) / 
+        tan_step <- lr * Reduce(`+`, old_tan) /
             aux_sample$sample_size
         new_ref_pt <- sample$metric$exp(old_ref_pt, tan_step)
 
-        delta <- Matrix::norm(new_ref_pt - old_ref_pt, "F") / 
-                Matrix::norm(old_ref_pt, "F")
+        delta <- Matrix::norm(new_ref_pt - old_ref_pt, "F") /
+            Matrix::norm(old_ref_pt, "F")
 
-        new_tan_imgs <- relocate(old_ref_pt, new_ref_pt, old_tan, 
-            sample$metric) 
+        new_tan_imgs <- relocate(
+            old_ref_pt, new_ref_pt, old_tan,
+            sample$metric
+        )
 
         aux_sample <- CSample$new(
             tan_imgs = list(new_ref_pt, new_tan_imgs),
-            centered = FALSE, metric = sample$metric)
+            centered = FALSE, metric = sample$metric
+        )
     }
     sample$frechet_mean <- sample$tangent_handler$reference_point
 }
@@ -132,13 +141,14 @@ validate_metric <- function(metric) {
 validate_conns <- function(conns, tan_imgs, vec_imgs, centered) {
     if (!is.null(conns)) {
         if (!is.null(tan_imgs) || !is.null(vec_imgs)) {
-            stop("When initializing, if conns is not NULL, tan_imgs and vec_imgs must be NULL.")
+            stop("When initializing, if conns is not NULL, tan_imgs and vec_imgs must be NULL.") # nolint: line_length_linter.
         }
-        if (!is.null(centered)) { 
+        if (!is.null(centered)) {
             warning("If conns is not NULL, centered is ignored")
         }
-        class_flag <- conns |> 
-            purrr::map_lgl(\(x) inherits(x, "dppMatrix")) |> all()
+        class_flag <- conns |>
+            purrr::map_lgl(\(x) inherits(x, "dppMatrix")) |>
+            all()
         if (!class_flag) stop("conns must be a list of dppMatrix objects.")
     }
 }
@@ -166,10 +176,11 @@ validate_tan_imgs <- function(tan_imgs, vec_imgs, centered) {
         if (!is.list(tan_imgs[[2]])) {
             stop("The second element of tan_imgs must be a list.")
         }
-        class_flag <- tan_imgs[[2]] |> 
-            purrr::map_lgl(\(x) inherits(x, "dspMatrix")) |> all()
+        class_flag <- tan_imgs[[2]] |>
+            purrr::map_lgl(\(x) inherits(x, "dspMatrix")) |>
+            all()
         if (!class_flag) {
-            stop("The second element of tan_imgs must be a list of dspMatrix objects.")
+            stop("The second element of tan_imgs must be a list of dspMatrix objects.") # nolint: line_length_linter.
         }
     }
 }
@@ -196,6 +207,3 @@ validate_vec_imgs <- function(vec_imgs, centered) {
         stop("The second element of vec_imgs must be a matrix.")
     }
 }
-
-
-
