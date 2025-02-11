0ff5e9cfe2e3599ee0ccb4ff33e968aca6cbb9ff
Nicolas Escobar <50273150+nicoesve@users.noreply.github.com>
2025-02-11 17:16:03 +0000

    debugging

--- airm.R
+++ airm.R
@@ -44,7 +44,7 @@ airm_log <- function(sigma, lambda) {
 #' airm_exp(sigma, v)
 #' @export
 airm_exp <- function(sigma, v) {
-    validate_exp_args(sigma, lambda)
+    validate_exp_args(sigma, v)
 
     sigma_sqrt <- expm::sqrtm(sigma) |> Matrix::nearPD() |> _$mat
     sigma_sqrt_inv <- Matrix::solve(sigma_sqrt)
