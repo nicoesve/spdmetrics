62d4d14bc225629b30c32bb523233798e5e120e2
Nicolas Escobar <50273150+nicoesve@users.noreply.github.com>
2025-02-12 14:16:31 -0500

    assembling package

---
+++ R/spdmetrics-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom Matrix Matrix
+#' @importFrom methods as new
+#' @importFrom R6 R6Class
+## usethis namespace: end
+NULL
